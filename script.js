db7cd58515bf88a94af8eb7cec09f4d098706b3f
Nikolasyuri <nikolasyuri@hotmail.com>
2021-04-07 14:04:02 -0300

    Base responsiva

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky")
         }
-    })
+    });
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+    });
 });
